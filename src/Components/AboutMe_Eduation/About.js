fd75c750af528ec2fc71d122dfc23e565d2e0035
TylerSimmons212 <tylersimmons212@gmail.com>
2021-12-06 18:31:48 -0700

    Update About.js

--- src/Components/AboutMe_Eduation/About.js
+++ src/Components/AboutMe_Eduation/About.js
@@ -12,10 +12,10 @@ function About () {
                 <img src={selfimg} alt="selfimg" />
             </div>
                 <section className="about-text">
-                    <p>I graduated and was certified by <a href="https://devmountain.com" target="_blank" >DevMountain</a> in April of 2019 as a full stack developer.</p>
-                    <p>Thanks to the blessing of families I then had the oppurtunity to work closely with a couple different marketing agencies to work with their clients and start my rode to specialising in e-commerce development.</p>
-                    <p>I've always been a techy my whole life. Loving the details, advancements, and capabilities that technology has developed to be in the world I always knew that I wanted to be apart of it. As my brothers pursued careers in Marketing, they were big influences in my choice to pursue web development. </p>
-                    <p>Other then my excitement in following the latest news in technology developments. I'm also a big film junkie. I love a good story. That's part of the reason why I love UI developent and working with marketing teams to help tell a companies story. </p>
+                    <p>I graduated and was certified by <a href="https://devmountain.com" target="_blank" >DevMountain</a> in April of 2019 as a full-stack developer.</p>
+                    <p>Thanks to the blessing of a family, I have had the opportunity to work closely with different marketing agencies and their clients and start my road to specializing in e-commerce development.</p>
+                    <p>I've always been a techy my whole life. Loving the details, advancements, and capabilities that technology has developed to become, I always knew that I wanted to be a part of it. As my brothers pursued careers in Marketing, they were significant influences in my choice to pursue web development.</p>
+                    <p>Other than my excitement in following the latest news in technology developments. I'm also a big film junkie, and I love a good story. That's part of why I love UI development and working with marketing teams to help tell a company's story. </p>
                 </section>
             </div>
         </div>
