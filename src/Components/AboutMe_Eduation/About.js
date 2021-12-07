5985893b14361a84aba6068f3d1af0e39517dcdb
TylerSimmons212 <tylersimmons212@gmail.com>
2021-12-06 18:44:59 -0700

    another about me update

--- src/Components/AboutMe_Eduation/About.js
+++ src/Components/AboutMe_Eduation/About.js
@@ -12,7 +12,7 @@ function About () {
                 <img src={selfimg} alt="selfimg" />
             </div>
                 <section className="about-text">
-                    <p>I graduated and was certified by <a href="https://devmountain.com" target="_blank" >DevMountain</a> in April of 2019 as a full-stack developer.</p>
+                    <p>I graduated from and was certified by <a href="https://devmountain.com" target="_blank" >DevMountain</a> in April of 2019 as a full-stack developer.</p>
                     <p>Thanks to the blessing of family, I have had the opportunity to work closely with different marketing agencies and their clients which started my road to specializing in e-commerce development.</p>
                     <p>I've been a techy my whole life. I love the details, advancements, and capabilities of technology. I always knew that I wanted to be a part of it. My brothers pursued careers in Marketing which significant influenced my decision to pursue web development.</p>
                     <p>I'm also a big film junkie, and I love a good story. That's part of why I love UI development and working with marketing teams to help tell a company's story. </p>
